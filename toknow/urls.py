e133a8f01ca05cd6c2b8b8afb241907cc54fcac9
Francois <f.bouet@gmail.com>
2014-04-05 12:19:38 +0100

    Initial commit

---
+++ toknow/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', include('app.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+from django.conf.urls.static import static
+
+urlpatterns += static('/css/', document_root='app/static/css/')
+urlpatterns += static('/images/', document_root='app/static/images/')
+urlpatterns += static('/js/', document_root='app/static/js/')

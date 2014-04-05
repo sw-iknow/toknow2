2e9716c00486cc2419f81e3b3ee4891df44659b6
Francois <f.bouet@gmail.com>
2014-04-05 18:20:48 +0100

    Create user on registration

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 import logging
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.template import RequestContext, loader
 from django.contrib import auth
+from app.models import UserInfo
+from django.contrib.auth.models import User
 
 logger = logging.getLogger(__name__)
 fh = logging.FileHandler('debug.log')
@@ -37,7 +39,21 @@ def registration(request):
     if request.user.is_active:
         return HttpResponse("Already registered")
     else:
+        email = request.POST.get('email', '')
+        password = request.POST.get('password', '')
+        message = request.POST.get('message', '')
+
+        if email:
+            u = User.objects.create_user(username=email, email=email, password=password)
+            u.save()
+            user = auth.authenticate(username=email, password=password)
+            ui = UserInfo(message=message)
+            ui.save()
+            auth.login(request, user)
+            return redirect("/")
+
         template = loader.get_template('app/registration.html')
         context = RequestContext(request, {'current_user': str(request.user)})
-        logger.debug("User: " + str(request.user))
+        logger.debug("Data: " + str(email) + " " + str(password) + " " + str(message))
+
         return HttpResponse(template.render(context))

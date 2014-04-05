4c734736ffbfadea42f96b334b3e73daea01e4fa
Francois <f.bouet@gmail.com>
2014-04-05 16:57:09 +0100

    Add authentication functionality

--- app/views.py
+++ app/views.py
@@ -1,5 +1,33 @@
+import logging
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.template import RequestContext, loader
+from django.contrib import auth
+
+logger = logging.getLogger(__name__)
+fh = logging.FileHandler('debug.log')
+msg = "%(asctime)s - %(levelname)s - %(message)s"
+formatter = logging.Formatter(msg)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.setLevel(logging.DEBUG)
 
 def index(request):
-    return render(request, 'app/index.html')
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    logout = request.POST.get('action', '')
+    
+    if logout:
+        logger.debug("Logging out!")
+        auth.logout(request)
+    else:
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+
+    template = loader.get_template('app/index.html')
+    context = RequestContext(request, {'current_user': str(request.user)})
+    logger.debug("User: " + str(request.user))
+    return HttpResponse(template.render(context))
--- toknow/urls.py
+++ toknow/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.auth.views import login, logout
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', include('app.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    (r'^accounts/logout/$', logout),
 )
 
 from django.conf.urls.static import static
